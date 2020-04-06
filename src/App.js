1370abe920ec87a679c417417a8998f559992efb
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-06 09:51:55 -0400

    implemented ability to sign up with email and password in firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
@@ -19,10 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        })
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
